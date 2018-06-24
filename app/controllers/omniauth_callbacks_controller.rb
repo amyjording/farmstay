93152f8958263c81118b8dfa3584f393199021ac
amyjording <zanizm@gmail.com>
2018-06-24 13:22:40 -0700

    Debug Omniauth providers

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -15,20 +15,17 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     redirect_to root_path
   end
 
-  def twitter
+#  def twitter
     # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @farmie = Farmie.from_omniauth(request.env["omniauth.auth"])
+#    @farmie = Farmie.from_omniauth(request.env["omniauth.auth"])
 
-    if @farmie.persisted?
-      sign_in_and_redirect @farmie, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Twitter") if is_navigational_format?
-    else
-      session["devise.twitter_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
-  end
+#    if @farmie.persisted?
+#      sign_in_and_redirect @farmie, event: :authentication #this will throw if @user is not activated
+#      set_flash_message(:notice, :success, kind: "Twitter") if is_navigational_format?
+#    else
+#      session["devise.twitter_data"] = request.env["omniauth.auth"]
+#      redirect_to new_user_registration_url
+#    end
+#  end
 
-  def failure
-    redirect_to root_path
-  end
 end
